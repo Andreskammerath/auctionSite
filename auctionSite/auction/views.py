ae8140f11458ab3895398e459d579ce070027338
Andres Kammerath <andreskammerath@gmail.com>
2020-07-29 22:32:29 -0300

    Starting to implement login logic

--- auctionSite/auction/views.py
+++ auctionSite/auction/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
-	return HttpResponse("Bienvenido a subastas Kammerath")
+	#if not request.user.is_authenticated:
+	return render(request, "auction/index.html")
 
 def login_view(request):
 	pass
