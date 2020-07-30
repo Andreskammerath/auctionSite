949f71e12406c32e21f7040e84e83d2f2ab4c729
Andres Kammerath <andreskammerath@gmail.com>
2020-07-29 22:57:48 -0300

    Login functionality all set

--- auctionSite/auction/views.py
+++ auctionSite/auction/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
@@ -7,7 +8,26 @@ def index(request):
 	return render(request, "auction/index.html")
 
 def login_view(request):
-	pass
+	#if submitting the form
+	if request.method == 'POST':
+		#get username submited
+		username = request.POST["username"]
+		#ger pass submited
+		password = request.POST["password"]
+		#authenticate it with django bult-in feature
+		user_ok = authenticate(request, username=username, password=password)
+		#if user is ok
+		if user_ok:
+			#login
+			login(request, user_ok)
+			#if redirect to index
+			return HttpResponseRedirect(reverse('index'))
+		else:
+			#else keep it in login and display error 
+			return render(request, "auction/login.html", {
+				message: "Credenciales Invalidas"
+				})	
+	return render(request, "auction/login.html")
 
 def logout_view(request):
 	pass
